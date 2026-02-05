69d58bad8f64b2d220970f8c1a3543688784688b
AnsariUvesh <ansariuvesh5780@gmail.com>
2026-02-05 14:54:09 +0530

    Task -4

---
+++ Vpc.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "strapi-vpc-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "${var.aws_region}a"
+}
